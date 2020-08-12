367f7cc376b6ab46b1c803c4be3603c866d8e65d
asedlack <asedlackcode@gmail.com>
2020-08-11 18:22:33 -0700

    Updates

---
+++ precache-manifest.5e5166085f04e8d543a9f7d783656143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78aa6e4abe91188dad624a887687670",
+    "url": "/detailingcars/index.html"
+  },
+  {
+    "revision": "4a37a8a1defa9b98b5a3",
+    "url": "/detailingcars/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9021dd84a70e3cf4f8f",
+    "url": "/detailingcars/static/js/2.bf89705e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/detailingcars/static/js/2.bf89705e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a37a8a1defa9b98b5a3",
+    "url": "/detailingcars/static/js/main.405b21d0.chunk.js"
+  },
+  {
+    "revision": "bd31064a8a42a86d1f8f",
+    "url": "/detailingcars/static/js/runtime-main.78595580.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/detailingcars/static/media/logo.5d5d9eef.svg"
+  }
+]);
